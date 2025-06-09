e7b38d20bc8c86eb54d3e78ea0c2466adb3b594b
prasoonk1204 <prasoonkumar467@gmail.com>
2025-06-10 00:30:00 +0530

    Add Vercel Analytics integration to Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/home/Navbar"
 import { Outlet } from "react-router"
+import { Analytics } from "@vercel/analytics/react";
 
 const Home = () => {
   return (
     <div className="bg-gradient-to-br from-zinc-800 to-zinc-950 relative h-screen min-h-screen p-4 flex flex-col gap-4 pt-36 sm:pt-20 font-mono">
       <Navbar />
       <Outlet />
+      <Analytics />
     </div>
   );
 }
